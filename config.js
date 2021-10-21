48424de8296b9bf8b67e70fb72e4754ae7d982cd
meghask14 <72038088+meghask14@users.noreply.github.com>
2021-10-21 17:14:09 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAxpjc_Emsc1kT89FJnNHZ3pnSoAJU4z0Y",
+  authDomain: "storryapp-1a204.firebaseapp.com",
+  databaseURL: "https://storryapp-1a204-default-rtdb.firebaseio.com",
+  projectId: "storryapp-1a204",
+  storageBucket: "storryapp-1a204.appspot.com",
+  messagingSenderId: "875333422300",
+  appId: "1:875333422300:web:0c584350c536f013e83819"
+};
